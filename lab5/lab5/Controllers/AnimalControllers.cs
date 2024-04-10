be3f0e9c3a7e9adffb446818e124b2d0c4180103
KausekK <jakub.kausa@gmail.com>
2024-04-10 19:56:30 +0200

    Stworzenie REST API

---
+++ lab5/lab5/Controllers/AnimalControllers.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using lab5.Database;
+using lab5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab5.Controllers;
+
+[ApiController]
+[Route("/controller/animals")]
+    public class AnimalControllers : ControllerBase
+    {
+
+        private readonly MockDb _mockDb;
+
+        public AnimalControllers(MockDb mockDb)
+        {
+            _mockDb = mockDb;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Animal>> GetAnimals()
+        {
+            return Ok(_mockDb.Animals);
+        }
+        
+    [HttpPost]
+    public IActionResult AddAnimals(Animal animal)
+    {
+        _mockDb.Animals.Add(animal);
+        return Created();
+        
+    }
+    
+    [HttpGet("{id}")]
+    public ActionResult<IEnumerable<Animal>> GetAnimal(int id)
+    {
+        var animal = _mockDb.Animals.Find(i => i.Id == id);
+        if (animal == null)
+        {
+            return NotFound();
+        }
+        return Ok(animal);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateAnimal(int id, Animal animal)
+    { 
+        if (id != animal.Id)
+        {
+            return BadRequest();
+        }
+        
+        var animalToUpdate = _mockDb.Animals.Find(i => i.Id == id);
+        if (animalToUpdate == null)
+        {
+            return NotFound();
+        }
+        
+        animalToUpdate.Name = animal.Name;
+        animalToUpdate.Category = animal.Category;
+        animalToUpdate.Weight = animal.Weight;
+        animalToUpdate.HairColor = animal.HairColor;
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteAnimal(int id)
+    {
+        var animal = _mockDb.Animals.Find(i => i.Id == id);
+        if (animal == null)
+        {
+            return NotFound();
+        }
+
+        _mockDb.Animals.Remove(animal);
+        return NoContent();
+    }
+
+    
+    }
--- lab5/lab5/Database/MockDb.cs
+++ lab5/lab5/Database/MockDb.cs
@@ -6,11 +6,23 @@ public class MockDb
 {
     public List<Animal> Animals { get; set; } = new List<Animal>();
 
+
     public MockDb()
     {
+        Animals.Add(new Animal { Id = 1, Name = "Pikachu", Category = "Elektryczny", HairColor = "Żółty", Weight = 30});
+        Animals.Add(new Animal { Id = 2, Name = "Bulbasaur", Category = "Trawa", HairColor = "Zielony", Weight = 20});
+        Animals.Add(new Animal { Id = 3, Name = "Charmander", Category = "Ogień", HairColor = "Pomarańczowy", Weight = 10});
+        
         Animals.Add(new Animal());
-        Animals.Add(new Animal());
-        Animals.Add(new Animal());
-        Animals.Add(new Animal());
+        Animals[0].Visits.Add(new Visit { Id = 1, VisitDate = DateTime.Now.AddDays(-10), visitDescription = "Badanie ogólne", VisitRating = 5 });
+        Animals[0].Visits.Add(new Visit { Id = 2, VisitDate = DateTime.Now.AddDays(-5), visitDescription = "Szczepienie", VisitRating = 3 });
+
+        Animals[1].Visits.Add(new Visit { Id = 1, VisitDate = DateTime.Now.AddDays(-15), visitDescription = "Badanie ogólne", VisitRating = 5 });
+        Animals[1].Visits.Add(new Visit { Id = 2, VisitDate = DateTime.Now.AddDays(-10), visitDescription = "Szczepienie", VisitRating = 3 });
+
+        Animals[2].Visits.Add(new Visit { Id = 1, VisitDate = DateTime.Now.AddDays(-20), visitDescription = "Badanie ogólne", VisitRating = 5 });
+        Animals[2].Visits.Add(new Visit { Id = 2, VisitDate = DateTime.Now.AddDays(-15), visitDescription = "Szczepienie", VisitRating = 4 });
+
     }
+    
 }
