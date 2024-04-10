2f2970a6dffb96452ce0a923aee73e4cc5ab3e45
KausekK <jakub.kausa@gmail.com>
2024-04-10 19:56:53 +0200

    dodanie wizyt

---
+++ lab5/lab5/Controllers/VisitControllers.cs
@@ -0,0 +1,40 @@
+﻿using lab5.Database;
+using lab5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab5.Controllers;
+[ApiController]
+[Route("/controller/visits")]
+public class VisitControllers : ControllerBase
+{
+    private readonly MockDb _mockDb;
+
+    public VisitControllers(MockDb mockDb)
+    {
+        _mockDb = mockDb;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<IEnumerable<Visit>> getVisitsForAnimal(int id)
+    {
+        var animal = _mockDb.Animals.FirstOrDefault(a => a.Id == id);
+        if (animal == null)
+        {
+            return NotFound();
+        }
+        return Ok(animal.Visits);
+    }
+
+    [HttpPost("{id}")]
+    public ActionResult<Visit> AddVisitForAnimal(int id, Visit visit)
+    {
+        var animal = _mockDb.Animals.FirstOrDefault(a => a.Id == id);
+        if (animal == null)
+        {
+            return NotFound();
+        }
+        visit.Id = animal.Visits.Count + 1;
+        animal.Visits.Add(visit);
+        return Created();
+    }
+}
---
+++ lab5/lab5/Models/Visit.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace lab5.Models;
+
+public class Visit
+{
+    public DateTime  VisitDate { get; set; }
+    public int Id { get; set; }
+    public string visitDescription { get; set; }
+    public int VisitRating { get; set; }
+
+}
